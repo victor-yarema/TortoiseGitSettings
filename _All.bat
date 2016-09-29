335b0de6f8e37a3dca0f1b9f3176427bbf95c8a5
Victor Yarema <victor.yarema@gmail.com>
2016-09-29 16:13:45 +0300

    Added "_All.bat".

---
+++ _All.bat
@@ -0,0 +1,12 @@
+
+regedit /s "00.00.Disabled hints about hierarchical configuration.reg"
+regedit /s "00.01.Disabled status cache.reg"
+regedit /s "00.02.Disabled icons overlays for fixed drives.reg"
+regedit /s "01.00.Hide menus for unversioned paths.reg"
+regedit /s "01.01.Disabled all ctx menu commands.reg"
+regedit /s "01.02.Enabled ctx menu commands ShowLog, CheckForModifications.reg"
+regedit /s "02.00.Disabled asterix log prefix.reg"
+regedit /s "02.01.Changed log font size from 8 to 10.reg"
+regedit /s "02.02.Enabled AbbreviateRenamings.reg"
+regedit /s "03.00.Progress dialog auto close.reg"
+pause
